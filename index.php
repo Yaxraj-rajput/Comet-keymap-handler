14a54690c6142085328fe596ffa1751612038fb8
yaxraj dabhi <76819134+Yaxraj-rajput@users.noreply.github.com>
2023-06-17 15:00:52 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$name= $_GET['name'];
+
+
+$id= $_GET['id'];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<form action="">
+
+<input type="text" value="<?php echo $name?>">
+<input type="text" value="<?php echo $id?>">
+</form>
+
+
+</body>
+</html>
